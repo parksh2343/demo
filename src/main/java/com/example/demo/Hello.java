a6918d83c3f6108a881d726ae250db8ffdf24160
parksh2343 <hydra2@naver.com>
2021-10-17 21:31:49 +0900

    스터디 관련 테스트11

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello world!!!!");
+        System.out.println("hello world!!");
 
         System.out.println("end program");
     }
