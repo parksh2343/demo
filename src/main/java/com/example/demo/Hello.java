c533863481e01f25571a9461c6676f9bcd17576c
parksh2343 <hydra2@naver.com>
2021-10-17 21:26:30 +0900

    스터디 관련 테스트2

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -4,4 +4,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+
+    public void temp(){
+        System.out.println("some msg");
+    }
 }
